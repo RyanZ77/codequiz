86c51ddb05eee173504c551d3117a5d80f81e0a7
Ryan Zumbrunnen <sirmacinator@gmail.com>
2020-05-07 23:00:25 -0500

    added questions

---
+++ questions.js
@@ -0,0 +1,40 @@
+var questions = [{
+	"question": "Commonly used data types DO NOT include:",
+	"option1": "Strings",
+	"option2": "Booleans",
+	"option3": "Alerts",
+	"option4": "Numbers",
+	"answer": 3
+}, 
+{
+	"question": "The condition in an if / else statement is enclosed within ____.",
+	"option1": "Quotes",
+	"option2": "Curly Brackets",
+	"option3": "Parentheses",
+	"option4": "Square Brackets",
+	"answer": 3
+}, 
+{
+	"question": "Arrays in JavaScript can be used to store ____.",
+	"option1": "Numbers and Strings",
+	"option2": "Other Arrays",
+	"option3": "Booleans",
+	"option4": "All of the above",
+	"answer": 4
+}, 
+{
+	"question": "String values must be enclosed within ____ when being assigned to variables.",
+	"option1": "Commas",
+	"option2": "Curly Brackets",
+	"option3": "Quotes",
+	"option4": "Parentheses",
+	"answer": 3
+}, 
+{
+	"question": "A very useful tool used during development and debugging for printing content to the debugger is:",
+	"option1": "Javascript",
+	"option2": "Terminal / Bash",
+	"option3": "For Loops",
+	"option4": "console.log",
+	"answer": 4
+}];	
